d6940f0f1970b41e8bc11451fb046373f01dda6b
estradae21 <usmc.estrada92@gmail.com>
2017-05-22 19:14:45 -0600

    Add files via upload

---
+++ ErnestoEstrada_1_09.java
@@ -0,0 +1,4 @@
+
+public class ErnestoEstrada_1_09 {
+
+}
---
+++ InteractiveAverageClass.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class InteractiveAverageClass {
+	
+	private double num1, num2;
+	Scanner console = new Scanner(System.in);
+	
+	public InteractiveAverageClass () {
+		num1 = 0;
+		num2 = 0;
+	}
+
+}
